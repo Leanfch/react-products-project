efaacadda49eea813b05060907e92ebb208eea5f
Leandro Fernandez <leanf94@gmail.com>
2025-05-28 12:32:24 -0300

    add product list component

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,22 @@
+import type { Product } from '../interface/ProductInterface.ts';
+import ProductCard from './ProductCard.tsx';
+
+interface ProductListProps {
+    products: Product[];
+    onProductSelect: (product: Product) => void;
+}
+
+const ProductList: React.FC<ProductListProps> = ({ products, onProductSelect }) => {
+    if (products.length === 0) {
+        return <p>No se encontraron productos</p>;
+    }
+    return (
+        <div className='productsListContainer'>
+            {products.map(product => (
+                <ProductCard key={product.id} product={product} onProductClick={onProductSelect}/>
+            ))}
+        </div>
+    )
+}
+
+export default ProductList
